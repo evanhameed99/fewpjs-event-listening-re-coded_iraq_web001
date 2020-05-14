2d523ad2120080f4e60db7e00cfb446f4d6ecf85
Evan hameed <evanhameed9966@gmail.com>
2020-05-14 17:05:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const input = document.getElementById('input');
-input.addEventListener('click', addingEventListener());
+input.addEventListener('click', addingEventListener)
 function addingEventListener() {
   alert(' i was clicked' );
 }
